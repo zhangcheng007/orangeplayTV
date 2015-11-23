d24d28a3ae1db570664d1c905b5ab00d77138d55
zhangcheng007 <zhang44540@163.com>
2015-11-23 20:59:16 +0800

    limit

--- control/info.js
+++ control/info.js
@@ -8,15 +8,30 @@ function Bilibili(page, pagecount) {
 
 module.exports =new Bilibili;
 
-Bilibili.prototype.get=function get(callback){
+Bilibili.prototype.getcurrentpage=function(currentpageoffset,callback){
 
- var queryarg='select * from bilibili limit 12';
+ var queryarg='select * from bilibili limit 16 offset '+currentpageoffset;
  db.createQuery(queryarg,function(err,result){
 
      if (err) {
        return  err;//console.error(err);
      };
+
+     
      callback(err,result);    //console.log(result.rows);
+ });
+}
+
+
+Bilibili.prototype.getcount=function(callback){
+
+ var queryarg='select * from bilibili';
+ db.createQuery(queryarg,function(err,result){
+
+     if (err) {
+       return  err;//console.error(err);
+     };
+     callback(err,result.rows.length);    //console.log(result.rows);
  });
 }
 
