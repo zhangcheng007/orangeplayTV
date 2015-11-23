4d68b11309e94e103797c683f23495545bd0ab37
zhangcheng007 <zhang44540@163.com>
2015-11-23 20:57:31 +0800

    add paging pagecount

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,41 @@ var info=require('../control/info.js');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-   info.get(function(err,result){
+  //分页
+    //var currentPage=1;
+    var currentPage = req.query.page?req.query.page:1;
+    var pagesize =16;
+   
+    var totalcount=0;
+    var pageCount=0;
+
+    var currentpageoffset=pagesize*(currentPage-1);
+
+
+    info.getcurrentpage(currentpageoffset,function(err,result){
 
      if (err) {
      	//console.error(err);
-     	res.send(err);
+     	res.send('search page err:'+err);
      };
- 
-   	 res.render('index', { title: 'OnzhangplayTV','tv':result.rows});
-   })
+       
+
+    info.getcount(function(err,count){
+         if (err) {
+     	  //console.error(err);
+     	  res.send('search count err:'+err);
+         };
+        
+         totalcount=count;
+         pagecount=Math.ceil(totalcount/pagesize);
+         
+         res.render('index', 
+        { title: 'OrangeplayTV','tv':result.rows,pageSize:pagesize,pageCount:pagecount,currentPage:currentPage});
+       });
+
+    });
+    
+
 
 });
 
