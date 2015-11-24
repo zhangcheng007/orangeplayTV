aab3e98a5824e253fbf91d2bd094151320c89a83
zhangcheng007 <zhang44540@163.com>
2015-11-24 20:20:41 +0800

    test

--- db/pgaction.js
+++ db/pgaction.js
@@ -31,7 +31,6 @@ DB.prototype.createQuery=function(queryarg,callback){
      });
 }
 
-
 DB.prototype.createInsert=function(table,args,callback){
 
      this.pg.connect(this.conString, function(err, client, done) {
